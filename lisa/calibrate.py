fe8c2ee945eb19e034b7a2d07e4abedd4806e57a
kuannie1 <anne.ku4@gmail.com>
2019-03-13 16:16:14 -0400

    added calibration files

--- lisa/calibrate.py
+++ lisa/calibrate.py
@@ -1,29 +1,176 @@
 # -*- coding: utf-8 -*-
-""" This is where the magic happens (TODO fix this docstring)
+import pylogit as pl
+import pandas as pd
+import numpy as np
+from graph import *
+from data import StreetDataGenerator
+import random
+from collections import OrderedDict
+import sys
+from random import random, randrange, choice
+from nx_types import *
 
-This is where the linear regression analysis happens to calibrate the weights
-for a linear LTS methodology
+def calc_distance_efficiency(edgenode1: NodeID, edgenode2: NodeID, endnode: NodeID):
+    """Takes in an edge and the endpoint to calculate how well the edge choice leads to the endpoint
+        Does this by calculating the distance between:
+            - first point to the end
+            - second point to the end
+            - length of the edge
 
-Example:
-    TODO insert some examples if needed
-
-"""
-
-
-def linear_regression_it(blah: str, blah2: str, blah3: int):
-    """This is a single line describing function.
-
-    This is your more detailed breakdown of the functions inner workings if
-    necessary. This can be multiple lines, and is separated from the one liner
-    with a blank line.
-
+        returns ((first point to the end) - (second point to the end))/(length of edge)
     Args:
-        blah (str): a word and nothing more
-        blah2 (str): is blah even a word?
-        blah3 (int): blah3 is not a word
+        edgenode1 (NodeID): Starting node identifier for the given edge
+        edgenode2 (NodeID): Ending node identifier for the given edge
+        endnode (NodeID): Final node identifier for the last node in the path
 
     Returns:
-        bool: The return value. True for success, False otherwise.
-
+        efficiency_calculation (float): numerical representation of the progress towards the ending node
     """
-    pass
+    
+    (edgenode1_x, edgenode1_y) = G.nodes[edgenode1]['x'], G.nodes[edgenode1]['y']
+    (edgenode2_x, edgenode2_y) = G.nodes[edgenode2]['x'], G.nodes[edgenode2]['y']
+    (endnode_x, endnode_y) = G.nodes[endnode]['x'], G.nodes[endnode]['y']
+    
+    distance_edgenode1_edgenode2 = np.sqrt(np.square(edgenode1_x - edgenode2_x) + np.square(edgenode1_y - edgenode2_y))
+    distance_edgenode1_end = np.sqrt(np.square(edgenode1_x - endnode_x) + np.square(edgenode1_y - endnode_y))
+    distance_edgenode2_end = np.sqrt(np.square(endnode_x - edgenode2_x) + np.square(endnode_y - edgenode2_y))
+
+    progress_to_end = distance_edgenode1_end - distance_edgenode2_end
+    
+    efficiency_calculation = (progress_to_end/distance_edgenode1_edgenode2)
+    return efficiency_calculation
+
+def find_attribute_dict(G: nx.DiGraph, node1: NodeID, node2: NodeID, endnode: NodeID):
+    """Returns the attributes of an edge from node1 to node2 in the context of G and endnode
+    
+    Args:
+        G (nx.DiGraph): DiGraph representation of the road network
+        node1 (NodeID): Starting node identifier for the given edge
+        node2 (NodeID): Ending node identifier for the given edge
+        endnode (NodeID): Final node identifier for the last node in the path to help with distance calculation
+    
+    Returns:
+        attribute_dict (dict): 
+    """
+    edgeID = (node1, node2)
+    attribute_dict = dict(G.edges[edgeID])
+    attribute_dict["distance_efficiency"] = calc_distance_efficiency(node1, node2, endnode)
+    
+    return attribute_dict
+    
+def create_dataframe(G, paths, feature_list):
+    """
+    Loops through each node in the path and gets the considered features in the dataframe
+    """
+    n_feats = len(feature_list)
+    choice_features = []
+    observation_ids = []
+    choice_indicators = []
+    choice_ids = []
+    counter = 0
+
+    for path_index in range(len(paths)):
+        
+        path = paths[path_index]
+
+        for i in range(len(path)-1):
+
+            # append all the edge options to a list of lists called choice_features
+            current_node = path[i]
+            end_node = path[-1]
+            neighbors = list(G.neighbors(current_node))
+            n_choices = len(neighbors)
+            observation_ids.append(counter*np.ones((n_choices,))) 
+            # 'i' is the "index" of the observation, or the reason why we know which observation is which
+            for neighbor in neighbors:
+                current_attribute_dict = find_attribute_dict(G, current_node, neighbor, end_node)
+                # iteratively adding each feature value to the observation
+                current_observation_choice_features = []
+                
+                for feature in feature_list:
+
+                    try:
+                        current_feature = current_attribute_dict[feature]
+                    except KeyError as e:
+                        current_feature = 0
+                    current_observation_choice_features.append(current_feature)
+                
+                choice_features.append(current_observation_choice_features)
+
+            # marking the choiceID's choice as '1' among zeros
+            choice_indicators.append(np.zeros((n_choices,)))
+            chosen = neighbors.index(path[i+1])
+            choice_indicators[-1][chosen] = 1
+            # All the possible choices out at this observation:
+            choice_ids.append(np.arange(n_choices))
+            
+            counter += 1
+
+    # preparing columns for the dataframe (long) format
+    overall_observation_ids = np.concatenate(observation_ids)
+    choice_features_overall = np.vstack(choice_features)
+    overall_choice_indicators = np.concatenate(choice_indicators)
+    overall_choice_ids = np.concatenate(choice_ids)
+
+    df = pd.DataFrame()
+
+    df['obs_ids'] = overall_observation_ids
+    df['choices'] = overall_choice_indicators
+    df['alt_ids'] = overall_choice_ids
+
+    # The next few lines just mean the columns will be consistent across choices
+    spec_names = OrderedDict()
+    specs = OrderedDict()
+    for i in range(n_feats):
+        spec = feature_list[i]
+        spec_names[spec] = spec
+        specs[spec] = 'all_same'
+        df[spec] = choice_features_overall[:,i]
+    return (df, n_feats, specs, spec_names)
+
+def create_model(dataframe, n_feats, specs, spec_names):
+    # Fit to a multinomial logit model (MNL)
+    choice_model = pl.create_choice_model(data=df,
+                                            alt_id_col='alt_ids',
+                                            obs_id_col='obs_ids',
+                                            choice_col='choices',
+                                            specification=specs,
+                                            model_type="MNL",
+                                            names=spec_names)
+    
+    # Specify the initial values and method for the optimization.
+    choice_model.fit_mle(np.zeros(n_feats), print_res = False)
+    fit_summary_print_output = choice_model.fit_summary
+    summary_print_output = choice_model.summary
+
+    summary = (summary_print_output.to_dict(orient="index"))
+
+    fit_summary = (fit_summary_print_output.to_dict())
+
+    choice_model.get_statsmodels_summary()
+
+    print(fit_summary_print_output)
+    print(summary_print_output)
+
+    return fit_summary, summary
+
+if __name__ == "__main__":
+
+    # bbox = Bbox(38.88300016, 38.878726840000006, -77.09939832, -77.10500768)
+    # G = Graph(bbox)
+    # G.save("boundgraph")
+
+    G = Graph.from_file("boundgraph").DiGraph
+    nodes = list(G.nodes)
+    paths = []
+    for i in range(3):
+        start = choice(nodes)
+        end = choice(nodes)
+        path = nx.shortest_path(G, start, end)
+        paths.append(path)
+    print("paths: ", paths)
+    featurelist = ["bike_lane", "separate_path", "speed_limit", "traffic_volume", "crosswalk", "turn", "distance_efficiency"]
+    (df, n_feats, specs, spec_names) = create_dataframe(G, paths, featurelist)
+    a, b = create_model(df, n_feats, specs, spec_names)
+    # print(a)
+    # print(b)
--- lisa/calibration_example.py
+++ lisa/calibration_example.py
@@ -2,18 +2,60 @@ import pylogit as pl
 import pandas as pd
 import numpy as np
 from graph import *
+from data import StreetDataGenerator
 import random
 from collections import OrderedDict
 import sys
 from random import random, randrange, choice
+from nx_types import *
 
+def calc_distance_efficiency(edgenode1: NodeID, edgenode2: NodeID, endnode: NodeID):
+    """Takes in an edge and the endpoint to calculate how well the edge choice leads to the endpoint
+        Does this by calculating the distance between:
+            - first point to the end
+            - second point to the end
+            - length of the edge
 
-def find_attribute_dict(G, startNode, endNode):
+        returns ((first point to the end) - (second point to the end))/(length of edge)
+    Args:
+        edgenode1 (NodeID): Starting node identifier for the given edge
+        edgenode2 (NodeID): Ending node identifier for the given edge
+        endnode (NodeID): Final node identifier for the last node in the path
+
+    Returns:
+        efficiency_calculation (float): numerical representation of the progress towards the ending node
     """
-        Gets attributes of the edge between two nodes
+    
+    (edgenode1_x, edgenode1_y) = G.nodes[edgenode1]['x'], G.nodes[edgenode1]['y']
+    (edgenode2_x, edgenode2_y) = G.nodes[edgenode2]['x'], G.nodes[edgenode2]['y']
+    (endnode_x, endnode_y) = G.nodes[endnode]['x'], G.nodes[endnode]['y']
+    
+    distance_edgenode1_edgenode2 = np.sqrt(np.square(edgenode1_x - edgenode2_x) + np.square(edgenode1_y - edgenode2_y))
+    distance_edgenode1_end = np.sqrt(np.square(edgenode1_x - endnode_x) + np.square(edgenode1_y - endnode_y))
+    distance_edgenode2_end = np.sqrt(np.square(endnode_x - edgenode2_x) + np.square(endnode_y - edgenode2_y))
+
+    progress_to_end = distance_edgenode1_end - distance_edgenode2_end
+    
+    efficiency_calculation = (progress_to_end/distance_edgenode1_edgenode2)
+    return efficiency_calculation
+
+def find_attribute_dict(G: nx.DiGraph, node1: NodeID, node2: NodeID, endnode: NodeID):
+    """Returns the attributes of an edge from node1 to node2 in the context of G and endnode
+    
+    Args:
+        G (nx.DiGraph): DiGraph representation of the road network
+        node1 (NodeID): Starting node identifier for the given edge
+        node2 (NodeID): Ending node identifier for the given edge
+        endnode (NodeID): Final node identifier for the last node in the path to help with distance calculation
+    
+    Returns:
+        attribute_dict (dict): 
     """
-    edgeID = (startNode, endNode)
-    return dict(G.edges[edgeID])
+    edgeID = (node1, node2)
+    attribute_dict = dict(G.edges[edgeID])
+    attribute_dict["distance_efficiency"] = calc_distance_efficiency(node1, node2, endnode)
+    
+    return attribute_dict
     
 def create_dataframe(G, paths, feature_list):
     """
@@ -24,24 +66,34 @@ def create_dataframe(G, paths, feature_list):
     observation_ids = []
     choice_indicators = []
     choice_ids = []
+    counter = 0
 
     for path_index in range(len(paths)):
+        
         path = paths[path_index]
 
         for i in range(len(path)-1):
 
             # append all the edge options to a list of lists called choice_features
             current_node = path[i]
+            end_node = path[-1]
             neighbors = list(G.neighbors(current_node))
             n_choices = len(neighbors)
-            observation_ids.append(i*np.ones((n_choices,))) 
+            observation_ids.append(counter*np.ones((n_choices,))) 
             # 'i' is the "index" of the observation, or the reason why we know which observation is which
             for neighbor in neighbors:
-                current_attribute_dict = find_attribute_dict(G, current_node, neighbor)
+                current_attribute_dict = find_attribute_dict(G, current_node, neighbor, end_node)
                 # iteratively adding each feature value to the observation
                 current_observation_choice_features = []
+                
                 for feature in feature_list:
-                    current_observation_choice_features.append(current_attribute_dict[feature])
+
+                    try:
+                        current_feature = current_attribute_dict[feature]
+                    except KeyError as e:
+                        current_feature = 0
+                    current_observation_choice_features.append(current_feature)
+                
                 choice_features.append(current_observation_choice_features)
 
             # marking the choiceID's choice as '1' among zeros
@@ -50,6 +102,8 @@ def create_dataframe(G, paths, feature_list):
             choice_indicators[-1][chosen] = 1
             # All the possible choices out at this observation:
             choice_ids.append(np.arange(n_choices))
+            
+            counter += 1
 
     # preparing columns for the dataframe (long) format
     overall_observation_ids = np.concatenate(observation_ids)
@@ -63,28 +117,6 @@ def create_dataframe(G, paths, feature_list):
     df['choices'] = overall_choice_indicators
     df['alt_ids'] = overall_choice_ids
 
-    # print(df[['obs_ids','choices', 'alt_ids']])
-    #     obs_ids  choices  alt_ids
-    # 0       0.0      0.0        0
-    # 1       0.0      1.0        1
-    # 2       1.0      1.0        0
-    # 3       2.0      0.0        0
-    # 4       2.0      1.0        1
-    # 5       3.0      1.0        0
-    # 6       4.0      1.0        0
-    # 7       4.0      0.0        1
-    # 8       5.0      1.0        0
-    # 9       6.0      1.0        0
-    # 10      6.0      0.0        1
-    # 11      7.0      1.0        0
-    # 12      8.0      0.0        0
-    # 13      8.0      1.0        1
-    # 14      8.0      0.0        2
-
-    # Ignore the leftmost column. As you see, each observation is a "choice". 
-    # The # of rows w/ the same observation = # of choices the biker makes
-    # The choices are either 1 or 0. Each observation makes a 
-
     # The next few lines just mean the columns will be consistent across choices
     spec_names = OrderedDict()
     specs = OrderedDict()
@@ -95,8 +127,8 @@ def create_dataframe(G, paths, feature_list):
         df[spec] = choice_features_overall[:,i]
     return (df, n_feats, specs, spec_names)
 
-def create_model(dataframe,n_feats, specs, spec_names):
-    # Estimate the multinomial logit model (MNL)
+def create_model(dataframe, n_feats, specs, spec_names):
+    # Fit to a multinomial logit model (MNL)
     choice_model = pl.create_choice_model(data=df,
                                             alt_id_col='alt_ids',
                                             obs_id_col='obs_ids',
@@ -104,34 +136,22 @@ def create_model(dataframe,n_feats, specs, spec_names):
                                             specification=specs,
                                             model_type="MNL",
                                             names=spec_names)
-
+    
     # Specify the initial values and method for the optimization.
-    choice_model.fit_mle(np.zeros(n_feats))
+    choice_model.fit_mle(np.zeros(n_feats), print_res = False)
+    fit_summary_print_output = choice_model.fit_summary
+    summary_print_output = choice_model.summary
 
-    # Look at the estimation results
-    # a = choice_model.get_statsmodels_summary()
+    summary = (summary_print_output.to_dict(orient="index"))
 
+    fit_summary = (fit_summary_print_output.to_dict())
 
-    cmfs = choice_model.fit_summary
-    cms = choice_model.summary
+    choice_model.get_statsmodels_summary()
 
+    print(fit_summary_print_output)
+    print(summary_print_output)
 
-    summary = (cms.to_dict(orient="index"))
-    # print("=============")
-    # print(type(b))
-    fit_summary = (cmfs.to_dict())
-    # Look at other all results at the same time
-    # choice_model.print_summaries()
-
-
-
-    # Look at the general and goodness of fit statistics
-    # choice_model.fit_summary
     return fit_summary, summary
-
-
-
-
 
 if __name__ == "__main__":
 
@@ -139,15 +159,17 @@ if __name__ == "__main__":
     # G = Graph(bbox)
     # G.save("boundgraph")
 
-    G = Graph.from_file("boundgraph")
-    nodes = list(G.DiGraph.nodes)
-    # print(type(nodes))
-    start = 131 #choice(nodes)
-    end = 354 #choice(nodes)
-    # print("start, end: ", start, end)
-    path = nx.shortest_path(G.DiGraph, start, end)
-    print("path: ", path)
-    (df, n_feats, specs, spec_names) = create_dataframe(G.DiGraph, [path], ["traffic_volume", "speed_limit"])
+    G = Graph.from_file("boundgraph").DiGraph
+    nodes = list(G.nodes)
+    paths = []
+    for i in range(3):
+        start = choice(nodes)
+        end = choice(nodes)
+        path = nx.shortest_path(G, start, end)
+        paths.append(path)
+    print("paths: ", paths)
+    featurelist = ["bike_lane", "separate_path", "speed_limit", "traffic_volume", "crosswalk", "turn", "distance_efficiency"]
+    (df, n_feats, specs, spec_names) = create_dataframe(G, paths, featurelist)
     a, b = create_model(df, n_feats, specs, spec_names)
-    print(a)
-    print(b)
+    # print(a)
+    # print(b)
