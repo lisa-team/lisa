8cb79186e3abdf7ec275633e30862e4a562ed5b5
Leon Jun Wei Lam <llam@olin.edu>
2019-04-03 13:40:13 -0400

    made everything into functions so you can input any .gdb file and its node/edge layers to get a graph

--- lisa/mire_check.py
+++ lisa/mire_check.py
@@ -26,11 +26,89 @@ def get_attributes_from_grade(grade):
     return attrs
 
 
+def mcoords_to_xy(mire_mcoords):
+    return fiona.transform.transform(
+        'EPSG:26985', 'EPSG:4326',
+    [mire_mcoords[0]],
+    [mire_mcoords[1]]
+    ) 
 
+
+def get_nodes_from_mire(gdb: str, node_layer: int):
+    err = 0
+    mire_coords = []
+    mire_nodes = []
+    with fiona.open(gdb, 'r', layer=node_layer) as inp:
+        for f in inp:
+            if f['properties']['INTERSECTIONID'][0] == '0':
+                err+=1
+                continue
+
+            mire_mcoords = f['geometry']['coordinates']
+            if mire_mcoords:
+
+                mire_xy = mcoords_to_xy(mire_mcoords)
+
+                if mire_xy[0][0] and mire_xy[1][0] and f['properties']['INTERSECTIONID'] != 'DCBoundary':
+
+                    mire_nodes.append((f['properties']['INTERSECTIONID'], {"x":mire_xy[0][0],"y":mire_xy[1][0], 'attributes':get_attributes_from_grade(f['properties']['GRADE'])}))
+                else:
+                    err+=1
+            else:
+                err+=1
+    return mire_nodes
+
+def get_edges_from_mire(gdb: str, edge_layer: int):
+    mire_edges = []
+    err = 0
+    with fiona.open(gdb, 'r', layer=edge_layer) as inp:
+        for f in inp:
+            firstIntersection, secondIntersection = f['properties']['FromIntersectionID'], f['properties']['ToIntersectionID']
+            if firstIntersection[0] == '0' or secondIntersection[0] == '0' or firstIntersection == "None" or secondIntersection == "None" or firstIntersection == 'DCBoundary' or secondIntersection == 'DCBoundary':
+                err +=1
+                continue
+            try:
+                edge = (firstIntersection,secondIntersection, dict(f['properties']))
+                mire_edges.append(edge)
+            except:
+                err+=1
+    return mire_edges
+
+def get_initial_graph_from_mire(gdb: str, node_layer: int, edge_layer: int):
+    init_graph = nx.DiGraph()
+
+    mire_nodes = get_nodes_from_mire(gdb, node_layer)
+    init_graph.add_nodes_from(mire_nodes)
+
+    mire_edges = get_edges_from_mire(gdb, edge_layer)
+    init_graph.add_edges_from(mire_edges)
+
+    return init_graph
+
+
+def get_expanded_graph_from_mire(gdb: str, node_layer: int, edge_layer: int):
+    init_graph = get_initial_graph_from_mire(gdb, node_layer, edge_layer)
+    expanded_graph = Graph(bound = None, mire_graph = init_graph)
+    return expanded_graph
 
 
 if __name__ == "__main__":
-    filename = "WashingtonDCGraph.pkl"
+
+
+    gdb = "scratch_022819.gdb"
+    node_layer = 3
+    edge_layer = 2
+
+    expanded_graph = get_expanded_graph_from_mire(gdb, node_layer, edge_layer)
+
+    print("expanded_graph nodes: ", list(expanded_graph.DiGraph.nodes(data=True))[0])
+    print("expanded_graph edges: ", list(expanded_graph.DiGraph.edges(data=True))[:10])
+
+    expanded_graph.plot_graph()
+
+
+
+    # filename = "WashingtonDCGraph.pkl"
 
     # get osmnx node coords
     # if not os.path.exists(filename):
@@ -49,75 +127,75 @@ if __name__ == "__main__":
 
 
     # get mire node coords
-    mire_coords = []
-    mire_nodes = []
-    mire_intersectionID_to_nodes = {}
-    temp = True
-    with fiona.open("scratch_022819.gdb", 'r', layer=3) as inp:
-        for f in inp:
-            if temp:
-                print(f)
-                # print(f['properties']['INTERSECTIONID'])
-                # print(f['properties']['GRADE'])
-                # print(inp)
-                temp = False
+    # mire_coords = []
+    # mire_nodes = []
+    # mire_intersectionID_to_nodes = {}
+    # temp = True
+    # with fiona.open("scratch_022819.gdb", 'r', layer=3) as inp:
+    #     for f in inp:
+    #         if temp:
+    #             print(f)
+    #             # print(f['properties']['INTERSECTIONID'])
+    #             # print(f['properties']['GRADE'])
+    #             # print(inp)
+    #             temp = False
 
-            if f['properties']['INTERSECTIONID'] == '00001':
-                continue
-            mire_coords.append(f['geometry']['coordinates'])
+    #         if f['properties']['INTERSECTIONID'] == '00001':
+    #             continue
+    #         mire_coords.append(f['geometry']['coordinates'])
 
-            mire_mcoords = f['geometry']['coordinates']
-            if mire_mcoords:
+    #         mire_mcoords = f['geometry']['coordinates']
+    #         if mire_mcoords:
 
-                mire_xy = fiona.transform.transform(
-                    'EPSG:26985', 'EPSG:4326',
-                [mire_mcoords[0]],
-                [mire_mcoords[1]]
-                )
+    #             mire_xy = fiona.transform.transform(
+    #                 'EPSG:26985', 'EPSG:4326',
+    #             [mire_mcoords[0]],
+    #             [mire_mcoords[1]]
+    #             )
 
-                if mire_xy[0][0] and mire_xy[1][0] and f['properties']['INTERSECTIONID'] != 'DCBoundary':
+    #             if mire_xy[0][0] and mire_xy[1][0] and f['properties']['INTERSECTIONID'] != 'DCBoundary':
 
-                    mire_nodes.append((f['properties']['INTERSECTIONID'], {"x":mire_xy[0][0],"y":mire_xy[1][0], 'attributes':get_attributes_from_grade(f['properties']['GRADE'])}))
-                else:
-                    print( mire_xy[0][0], mire_xy[1][0])
+    #                 mire_nodes.append((f['properties']['INTERSECTIONID'], {"x":mire_xy[0][0],"y":mire_xy[1][0], 'attributes':get_attributes_from_grade(f['properties']['GRADE'])}))
+    #             else:
+    #                 print(mire_xy[0][0], mire_xy[1][0])
 
-    # print(mire_intersectionID_to_nodes)
+    # # print(mire_intersectionID_to_nodes)
 
-    print("nodes compiled")
+    # print("nodes compiled")
 
-    g1 = nx.DiGraph()
-    g1.add_nodes_from(mire_nodes)
+    # g1 = nx.DiGraph()
+    # g1.add_nodes_from(mire_nodes)
 
 
-    print([k for k in g1.nodes(data=True) if k[1].get("x") is None][:10])
+    # print([k for k in g1.nodes(data=True) if k[1].get("x") is None][:10])
 
-    print("node adding done")
+    # print("node adding done")
 
-    mire_edges = []
-    temp = True
-    err = 0
-    with fiona.open("scratch_022819.gdb", 'r', layer=2) as inp:
-        for f in inp:
-            if temp:
-                print(f)
-                temp = False
-            firstIntersection, secondIntersection = f['properties']['FromIntersectionID'], f['properties']['ToIntersectionID']
-            if firstIntersection[0] == '0' or secondIntersection[0] == '0' or firstIntersection == "None" or secondIntersection == "None" or firstIntersection == 'DCBoundary' or secondIntersection == 'DCBoundary':
+    # mire_edges = []
+    # temp = True
+    # err = 0
+    # with fiona.open("scratch_022819.gdb", 'r', layer=2) as inp:
+    #     for f in inp:
+    #         if temp:
+    #             print(f)
+    #             temp = False
+    #         firstIntersection, secondIntersection = f['properties']['FromIntersectionID'], f['properties']['ToIntersectionID']
+    #         if firstIntersection[0] == '0' or secondIntersection[0] == '0' or firstIntersection == "None" or secondIntersection == "None" or firstIntersection == 'DCBoundary' or secondIntersection == 'DCBoundary':
 
-                err +=1
-                continue
-            try:
-                edge = (firstIntersection,secondIntersection, dict(f['properties']))
-                mire_edges.append(edge)
-                # print(edge)
-            except:
-                print("problem number", err)
-                err+=1
-    # print("errors:", err)
+    #             err +=1
+    #             continue
+    #         try:
+    #             edge = (firstIntersection,secondIntersection, dict(f['properties']))
+    #             mire_edges.append(edge)
+    #             # print(edge)
+    #         except:
+    #             print("problem number", err)
+    #             err+=1
+    # # print("errors:", err)
 
-    # print("edges compiled")
+    # # print("edges compiled")
 
-    g1.add_edges_from(mire_edges)
+    # g1.add_edges_from(mire_edges)
 
 
     # print("edge adding done")
@@ -129,18 +207,18 @@ if __name__ == "__main__":
 
     # print([k for k in g1.nodes(data=True) if k[1].get("x") is None][:10])
 
-    g2 = Graph(bound = None, mire_graph = g1)
+    # g2 = Graph(bound = None, mire_graph = g1)
 
-    print("g2 init nodes: ", list(g2.init_graph.nodes(data=True))[0])
-    print("g2 init edges: ", list(g2.init_graph.edges(data=True))[0])
+    # print("g2 init nodes: ", list(g2.init_graph.nodes(data=True))[0])
+    # print("g2 init edges: ", list(g2.init_graph.edges(data=True))[0])
 
-    print("g2 nodes: ", list(g2.DiGraph.nodes(data=True))[0])
-    print("g2 edges: ", list(g2.DiGraph.edges(data=True))[:10])
-    # print("g2 node map: ", g2.node_map)
+    # print("g2 nodes: ", list(g2.DiGraph.nodes(data=True))[0])
+    # print("g2 edges: ", list(g2.DiGraph.edges(data=True))[:10])
+    # # print("g2 node map: ", g2.node_map)
 
 
 
-    g2.plot_graph()
+    # g2.plot_graph()
 
 
 
